3dffc40766f923ee8bbbb019069f0455da575339
m2lawrence <xhtml@live.co.uk>
2016-02-21 18:32:03 +0000

    C# Basic Algorithms and Input Usage
    This C# programme allows the user to input their name, the system reads
    the input and stores it in the name variable and uses it in the
    programme along with the time api and then writes them onto the screen.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppCSNameAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Console.WriteLine("Please enter your name Mike:");
+            string strName = Console.ReadLine();
+            Console.WriteLine("Hi {0}, the current time is {1}.", strName, System.DateTime.Now.TimeOfDay);
+            Console.WriteLine("To quit the program, press any key!");
+            Console.ReadKey(); } }
+}
